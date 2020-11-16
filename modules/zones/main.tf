75ef64c9cf544fcec4c8959670baccdb4ab07ad2
Dmytro Prokhorenkov <dmytro.prokhorenkov@daimler.com>
2020-11-16 14:18:01 +0100

    Added possibility to create private zones
    * Added code to create private zones
    * Bumped version for terraform pre-commit

--- modules/zones/main.tf
+++ modules/zones/main.tf
@@ -5,5 +5,9 @@ resource "aws_route53_zone" "this" {
   comment       = lookup(each.value, "comment", null)
   force_destroy = lookup(each.value, "force_destroy", false)
 
+  vpc {
+    vpc_id = lookup(each.value, "vpc_id", null)
+  }
+
   tags = lookup(each.value, "tags", null)
 }
